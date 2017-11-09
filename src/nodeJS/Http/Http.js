a839e41e00850dd47a3a99139825e24fcd6c954b
primeval1 <jimmyst@hotmail.gr>
2017-11-09 12:19:34 +0200

    added http server example

---
+++ src/nodeJS/Http/Http.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Dimitris on 11/9/2017.
+ */
+
+//a dead simple example on http
+let http = require('http');
+
+
+http.createServer(function (req, res) {
+//we set the response header on "textplain, and status code 200
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    //then we append to the body the text we want
+    res.end('Hello world \n');
+}).listen(1337, '127.0.0.1');
+//listen on 1337 on localhost ( http://localhost:1337)
